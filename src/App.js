e603746fca5180476613522de49aba5514a1bf97
dawn <mohammedishmam@gmail.com>
2024-01-07 20:34:00 -0500

    Info completed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Info from './components/Info.js';
+
+export default function App() {
+  return (
+    <div className="container">
+      <Info />
+    </div>
+  );
+}
---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import erenPfp from '../images/eren.jpg';
+
+export default function Info() {
+  return (
+    <div className="info-container">
+      <img src={erenPfp} class="pfp"/>
+      <h2 className="name">Eren Yeager</h2>
+      <p className="location">Paradis Island</p>
+      <small className="email">eren_yeager.com</small>
+      <button>Email</button>
+    </div>
+  );
+}
